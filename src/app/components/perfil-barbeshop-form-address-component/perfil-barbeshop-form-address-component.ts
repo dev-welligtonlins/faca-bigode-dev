376172f34a4da70f1e2432453aedb063b2668164
dev-welligtonlins <welligtonlinsemp@gmail.com>
2025-09-03 23:13:50 -0300

    buscando cep em perfil-barbershop-form-address

--- src/app/components/perfil-barbeshop-form-address-component/perfil-barbeshop-form-address-component.ts
+++ src/app/components/perfil-barbeshop-form-address-component/perfil-barbeshop-form-address-component.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { utilsSearchCEP } from '../../utils/utilsSearchCEP';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AddressModel } from '../../models/address-model';
+
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-perfil-barbeshop-form-address-component',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './perfil-barbeshop-form-address-component.html',
   styleUrl: './perfil-barbeshop-form-address-component.scss'
 })
 export class PerfilBarbeshopFormAddressComponent {
-  
+
+  form = new FormGroup({
+    cep: new FormControl<string>(''),
+    logradouro: new FormControl(''),
+    bairro: new FormControl(''),
+    cidade: new FormControl(''),
+    uf: new FormControl('')
+  })
+
+  constructor(private utilsSearchCep: utilsSearchCEP) {
+    // Observa mudanças no campo de CEP
+    this.form.get('cep')?.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged()
+    ).subscribe((cep) => this.buscarCep(cep as string));
+
+  }
+
+  buscarCep(cep: string) {
+    this.utilsSearchCep.consultarCep(cep).subscribe({
+      next: (dados: AddressModel) => {
+        if (!dados.erro) {
+          this.form.patchValue({
+            logradouro: dados.logradouro,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            uf: dados.uf
+          });
+        } else {
+          this.form.patchValue({
+            logradouro: '',
+            bairro: '',
+            cidade: '',
+            uf: ''
+          });
+        }
+      },
+      error: () => {
+        console.error('Erro ao consultar CEP');
+      }
+    });
+  }
 }
+
---
+++ src/app/models/address-model.ts
@@ -0,0 +1,13 @@
+export interface AddressModel {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+  erro?: boolean; // usado quando CEP não existe
+}
