e80969ad7bfdd76e8a3a3229db4acb5c92d95d43
dev-welligtonlins <welligtonlinsemp@gmail.com>
2025-11-05 08:35:17 -0300

    corrigindo - services.actives true barbershops

--- back-end/src/main/java/com/awsft/knifeandmustache/repository/ServiceRepository.java
+++ back-end/src/main/java/com/awsft/knifeandmustache/repository/ServiceRepository.java
@@ -14,7 +14,7 @@ public interface ServiceRepository extends JpaRepository<Service, Long>{
     List<Service> findByBarbershopIdAndServiceActiveTrue(Long id);
 
     @Query("SELECT new com.awsft.knifeandmustache.dto.ServiceDTO(s.serviceDescription, s.duration, s.value, s.serviceCategory) " +  
-        "FROM Service s JOIN s.barbershop bs" + " WHERE bs.id = :id GROUP BY s.serviceDescription, s.duration, s.value, s.serviceCategory")
+        "FROM Service s JOIN s.barbershop bs" + " WHERE s.serviceActive = true AND bs.id = :id GROUP BY s.serviceDescription, s.duration, s.value, s.serviceCategory")
     List<ServiceDTO> findServicesByBarbershopId(Long id);
 } 
 
